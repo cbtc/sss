c919bf8ef1aaec8bdab5d9c8d35523f26ff1c86d
cbtc <carlos_ddg@yahoo.es>
2018-08-04 16:52:22 +0200

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1017,12 +1017,12 @@ int64_t GetProofOfWorkReward(int64_t nFees)
 // miner's coin stake reward based on coin age spent (coin-days)
 int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
-    int64_t nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);
+            int64_t nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8);
 
-    if(nBestHeight == 5401)
-    {
-    nSubsidy = 1500000000  * COIN;
-    }
+            if(nBestHeight == 632373)
+            {
+            nSubsidy = 1500000000  * COIN;
+            }
 
     if (fDebug && GetBoolArg("-printcreation"))
         printf("GetProofOfStakeReward(): create=%s nCoinAge=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nCoinAge);
